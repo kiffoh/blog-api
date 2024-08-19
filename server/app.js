d2bec4b6dc06dac85d3f166220279ea60d36f5b2
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-08-19 10:53:07 +0100

    Update CORS to include netlify html

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://loquacious-marzipan-4eada2.netlify.app' }));
 app.options('*', cors());
 
 app.use(passport.initialize());
